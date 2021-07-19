d951e8a8495bbf2b651fb135798d750dc144b5d8
Pratik Kute <35337119+pratikkute@users.noreply.github.com>
2021-07-20 01:12:24 +0530

    minified html

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,41 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const htmlmin = require("html-minifier");
+
+
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy('./src/style.css');
-  eleventyConfig.addPassthroughCopy('./src/assets');
-  eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  eleventyConfig.addPlugin(syntaxHighlight);
+
+  // Minify HTML
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
+      output: "public",
+    },
   };
 }
