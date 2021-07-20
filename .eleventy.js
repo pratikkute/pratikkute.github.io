c8a9ad42c5c1959ed86e46b5bba91b3ebd619a4f
Pratik Kute <35337119+pratikkute@users.noreply.github.com>
2021-07-20 18:41:06 +0530

    added fav ico

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
